90718f3cf1eb35ff2fe1231ecf4a646bd9c6ec8f
subhamsoni <sonishubham65@gmail.com>
2020-09-09 16:50:00 +0530

    created true and false test cases

---
+++ test-cases/false.js
@@ -0,0 +1,22 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+let server = require('../app');
+var expect = chai.expect;
+
+describe("Testing basic api", () => {
+    context('Get:/:', () => {
+        it("1. First Hit", (done) => {
+            chai.request(server)
+                .get("/").then(response => {
+                    expect(response.statusCode).to.equal(200);
+                    expect(response.body).to.be.an('object').to.contain.all.keys('success', 'message');
+                    expect(response.body.message).to.be.a("string");
+                    expect(response.body.success).to.equal(0);
+                    done();
+                }).catch(err => {
+                    done(err)
+                })
+        })
+    })
+})
